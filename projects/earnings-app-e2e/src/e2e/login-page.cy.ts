0d68b6a7df09de8a2f0409586da24be665364cce
Mike Ryan <mike.ryan52@gmail.com>
2022-01-14 16:21:16 -0600

    06-interacting-with-elements

--- projects/earnings-app-e2e/src/e2e/login-page.cy.ts
+++ projects/earnings-app-e2e/src/e2e/login-page.cy.ts
@@ -25,4 +25,18 @@ describe('Login Page', () => {
 
     cy.get('form').find('input').should('have.length', 2);
   });
+
+  it('should log the user in when providing valid credentials', () => {
+    cy.visit('/');
+
+    cy.get('bco-login-form')
+      .find('[data-test-id="usernameInput"]')
+      .type('Admin');
+
+    cy.get('bco-login-form')
+      .find('[data-test-id="passwordInput"]')
+      .type('password');
+
+    cy.get('bco-login-form').find('[data-test-id="loginButton"]').click();
+  });
 });
