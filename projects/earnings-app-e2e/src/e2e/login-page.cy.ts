8ec3a40a43578ee2e1944585a5d6d9382cb2a6bf
Mike Ryan <mike.ryan52@gmail.com>
2022-01-14 15:53:28 -0600

    01-first-test

--- projects/earnings-app-e2e/src/e2e/login-page.cy.ts
+++ projects/earnings-app-e2e/src/e2e/login-page.cy.ts
@@ -1,1 +1,7 @@
-describe('Login Page', () => {});
+describe('Login Page', () => {
+  it('should show "Login" in the header', () => {
+    cy.visit('/');
+
+    cy.get('h3').contains('Login');
+  });
+});
