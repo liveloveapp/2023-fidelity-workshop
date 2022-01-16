141c1bb75f0849e7817317bd3058d2405e96f550
Mike Ryan <mike.ryan52@gmail.com>
2022-01-16 08:12:22 -0500

    18-find-by-test-id

--- projects/earnings-app-e2e/src/support/book-form-component.harness.ts
+++ projects/earnings-app-e2e/src/support/book-form-component.harness.ts
@@ -1,14 +1,12 @@
 import { BookModel, BookRequiredProps } from '@book-co/shared-models';
 
 export const getBookForm = () => cy.get('bco-book-detail');
-export const getNameInput = () =>
-  getBookForm().find('[data-test-id="nameInput"]');
+export const getNameInput = () => getBookForm().findByTestId('nameInput');
 export const getEarningsInput = () =>
-  getBookForm().find('[data-test-id="earningsInput"]');
+  getBookForm().findByTestId('earningsInput');
 export const getDescriptionInput = () =>
-  getBookForm().find('[data-test-id="descriptionInput"]');
-export const getSaveButton = () =>
-  getBookForm().find('[data-test-id="saveButton"]');
+  getBookForm().findByTestId('descriptionInput');
+export const getSaveButton = () => getBookForm().findByTestId('saveButton');
 
 export const fillForm = (book: BookRequiredProps) => {
   getNameInput().clear().type(book.name);
--- projects/earnings-app-e2e/src/support/commands.ts
+++ projects/earnings-app-e2e/src/support/commands.ts
@@ -3,9 +3,18 @@ declare namespace Cypress {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   interface Chainable<Subject> {
     getByTestId(id: string): Chainable<Element>;
+    findByTestId(id: string): Chainable<Element>;
   }
 }
 
 Cypress.Commands.add('getByTestId', (testId: string) => {
   return cy.get(`[data-test-id="${testId}"]`);
 });
+
+Cypress.Commands.add(
+  'findByTestId',
+  { prevSubject: 'element' },
+  (subject: Cypress.Chainable<Element>, testId: string) => {
+    return subject.find(`[data-test-id="${testId}"]`);
+  }
+);
