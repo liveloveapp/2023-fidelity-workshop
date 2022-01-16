59af2d3bdecaae43bfdb7101e7657023f89ee1fa
Mike Ryan <mike.ryan52@gmail.com>
2022-01-16 07:59:21 -0500

    14-editing-book

--- projects/earnings-app-e2e/src/support/book-list-component.harness.ts
+++ projects/earnings-app-e2e/src/support/book-list-component.harness.ts
@@ -1,2 +1,8 @@
 export const getBook = (bookId: string) =>
   cy.get(`[data-test-id="book-${bookId}"]`);
+export const getEditBookButton = (bookId: string) =>
+  getBook(bookId).find('[data-test-id="bookEditButton"]');
+
+export const clickEditButtonOnBook = (bookId: string) => {
+  getEditBookButton(bookId).click();
+};
